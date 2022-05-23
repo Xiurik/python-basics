5d075f52f3def90c7daf9951f5f118c41b5378ab
Irwin Romero <irwin.romero.rdz@gmail.com>
2022-05-23 17:39:46 -0500

    add:File IO

---
+++ basics/file.py
@@ -0,0 +1,43 @@
+"""
+my_file = open('file_test.txt', mode='r+')
+
+# Here we read the file
+print(my_file.read())
+
+# if we want to read the file again we need to do the seek
+my_file.seek(0)
+print(my_file.read())
+
+# Here we read line by line
+my_file.seek(0)
+print(my_file.readline())
+
+# Here we read all the lines
+my_file.seek(0)
+print(my_file.readlines())
+
+# Close the file
+my_file.close()
+"""
+
+# Here is the way to work with files and dont worry about closing it
+# w : allows us to write to the file
+# r : allows us to read from the file
+# r+ : allows us to read and write to the file
+# a : allows us to append to the file (write at the end)
+with open('file_test.txt', mode='a') as mf:
+    for i in range(0, 10):
+        text = mf.write(f'Hi {i}\n')
+
+with open('./basics/file_test2.txt', mode='r') as mf2:
+    print(mf2.read())
+
+try:
+    with open('12345.txt', mode='r') as mf:
+        print(mf.read())
+except FileNotFoundError as e:
+    print('File not found')
+    raise e
+except IOError as e:
+    print('IO Error')
+    raise e
---
+++ basics/regular_expressions.py
@@ -0,0 +1,30 @@
+import re
+
+text = 'Hello my friend, lets have some fun! Mr. RORI901006Q40'
+pattern = re.compile(r'\bf[a-z]*\b')
+pattern2 = re.compile(r'([a-zA-Z]{3,4})(\d{6})([a-zA-Z\d]{3})')
+exist = re.search('f', text)
+
+# tell us where the string occurs as a tuple (first one found)
+# print(exist.span())
+
+# when the text starts
+# print(exist.start())
+
+# show us where it ends
+# print(exist.end())
+
+# return the string where it was the match
+# print(exist.group())
+
+# search for the match
+print(pattern2.search(text))
+
+# return all the words that matches
+print(pattern2.findall(text))
+
+# return the match only if it matches the whole string
+print(pattern2.fullmatch(text))
+
+# return the match only if it matches the beginning of the string
+print(pattern2.match(text))
