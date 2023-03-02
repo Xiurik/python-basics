f7c4184ae678269bf2a02a843eb6f265af65beee
Irwin Romero <irwin.romero.rdz@gmail.com>
2023-03-02 13:10:41 -0600

    finish scrapping

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -1,11 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint as pp
-
-res = requests.get("http://news.ycombinator.com/news")
-soup = BeautifulSoup(res.text, "html.parser")
-links = soup.select(".titleline")
-votes = soup.select(".subtext")
+import time as t
 
 
 def sort_stories(hnlist):
@@ -14,14 +10,13 @@ def sort_stories(hnlist):
 
 def create_custom_hn(links, votes):
     hnl = []
-
     for idx, item in enumerate(links):
         score = votes[idx].select(".score")
         if len(score) == 0:
             continue
 
         cp = int(score[0].getText().replace(" points", ""))
-        if cp > 80:
+        if cp > 10:
             title = item.getText()
             href = item.a["href"]
             hnl.append({"title": title, "link": href, "votes": cp})
@@ -29,4 +24,27 @@ def create_custom_hn(links, votes):
     return sort_stories(hnl)
 
 
-pp.pprint(create_custom_hn(links, votes))
+def load_data(amount):
+    link = "http://news.ycombinator.com/news"
+    res = requests.get(link)
+    soup = BeautifulSoup(res.text, "html.parser")
+    links = soup.select(".titleline")
+    votes = soup.select(".subtext")
+    link_sources = []
+    votes_sources = []
+
+    if amount == 1:
+        return create_custom_hn(links, votes)
+    elif amount > 1:
+        link_sources = link_sources + links
+        votes_sources = votes_sources + votes
+        for i in range(2, amount):
+            res = requests.get(link + "?p=" + str(i))
+            soup = BeautifulSoup(res.text, "html.parser")
+            link_sources = link_sources + soup.select(".titleline")
+            votes_sources = votes_sources + soup.select(".subtext")
+            t.sleep(3)
+        return create_custom_hn(link_sources, votes_sources)
+
+
+pp.pprint(load_data(4))
